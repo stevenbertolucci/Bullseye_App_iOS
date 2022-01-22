da11041f98fa1999088b7a62644851739225a90c
Steven Bertolucci <13703308+stevenbertolucci@users.noreply.github.com>
2022-01-22 09:32:16 -0800

    Add files via upload

---
+++ BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Steven Bertolucci on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
